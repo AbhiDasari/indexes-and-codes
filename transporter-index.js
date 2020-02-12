1bece22462b46c90b485ad81d1caa4c7c973da54
Sri Sai Abhishake Gopal Dasari <srisaiabhishakegopal@gmail.com>
2020-02-12 17:41:30 +0530

    Update transporter-index.js

--- transporter-index.js
+++ transporter-index.js
@@ -1,1 +1,81 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = 4000;
 
+// Import all function modules
+const addToWallet = require('./1_addToWallet');
+const updateShipment = require('./2_updateShipment');
+const viewHistory = require('./3_viewHistory');
+const viewDrugCurrentState = require('./4_viewDrugCurrentState');
+
+
+// Define Express app settings
+app.use(cors());
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.set('title', 'Pharma Network App');
+
+app.get('/', (req, res) => res.send('Hello User'));
+
+
+app.post('/updateShipment', (req, res) => {
+    updateShipment.execute(req.body.buyerCRN, req.body.drugName, req.body.transporterCRN).then (() => {
+        console.log('Shipment Updated ');
+        const result = {
+            status: 'success',
+            message: 'Shipment Updated'
+        };
+        res.json(result);
+    })
+    .catch((e) => {
+        const result = {
+            status: 'error',
+            message: 'Failed',
+            error: e
+        };
+        res.status(500).send(result);
+    });
+});
+
+app.post('/viewHistory', (req, res) => {
+    viewHistory.execute(req.body.drugName, req.body.serialNo).then (() => {
+        console.log('History Retrived ');
+        const result = {
+            status: 'success',
+            message: 'History Retrived'
+        };
+        res.json(result);
+    })
+    .catch((e) => {
+        const result = {
+            status: 'error',
+            message: 'Failed',
+            error: e
+        };
+        res.status(500).send(result);
+    });
+});
+
+
+app.post('/viewDrugCurrentState', (req, res) => {
+    viewDrugCurrentState.execute(req.body.drugName, req.body.serialNo).then (() => {
+        console.log('Drug Current state retrived ');
+        const result = {
+            status: 'success',
+            message: 'Drug Current state retrived'
+        };
+        res.json(result);
+    })
+    .catch((e) => {
+        const result = {
+            status: 'error',
+            message: 'Failed',
+            error: e
+        };
+        res.status(500).send(result);
+    });
+});
+
+
+app.listen(port, () => console.log(`Distributed Registrar App listening on port ${port}!`));
